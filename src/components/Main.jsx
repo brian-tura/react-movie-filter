89e906f765a318e98332d7912635f1dd9f24fbeb
brian-tura <b.tura2002@gmail.com>
2025-05-13 09:47:25 +0200

    [FEAT] Options created

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,39 @@
 import React from 'react'
-import Films from './Films'
+import films from '../../filmList'
+import { useState, useEffect } from 'react'
 
 const Main = () => {
-  return (
-    <main>
-        <Films />
-    </main>
-  )
+    const [initialFilms, setInitialFilms] = useState(films);
+    const [filtereMovies, setFilteredMovies] = useState(initialFilms)
+    const [filmOptions, setFilmOptions] = useState([]);
+    const [genre, setGenre] = useState("");
+
+    return (
+        <main>
+            <select value={genre} onChange={(e) => setGenre(e.target.value)}>
+                <option value="">Seleziona il genere</option>
+                {initialFilms.map((film) => {
+                    return (
+                        <option value={film.genre}>{film.genre}</option>
+                    )
+                })}
+            </select>
+            <ul>
+                {initialFilms.map((film) => {
+                    return (
+                        <li>
+                            <p>
+                                {film.title}
+                            </p>
+                            <p>
+                                - {film.genre}
+                            </p>
+                        </li>
+                    )
+                })}
+            </ul>
+        </main>
+    )
 }
 
 export default Main
